6db00cdee9d6dc0d9014390c9a4ffa1f472287aa
Hiroshi Suda <suda@net.it-chiba.ac.jp>
2019-06-20 23:05:02 +0900

    PWA用のファイルを追記

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,22 @@
 // service-worker.js
+const cache_name = 'reminder-v1';
+var cache = [
+    '/',
+    '/manifest.json',
+    '/icon-192.png',
+    '/icon-256.png',
+    '/reminder.css',
+    '/reminder.js',
+    '/service-worker.js'
+];
 self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches
+            .open(cache_name)
+            .then(function(cache) {
+                return cache.addAll(cache);
+            })
+    );
     console.log('[ServiceWorker] Install');
 });
 
@@ -8,4 +25,12 @@ self.addEventListener('activate', function(e) {
 });
 
 // 現状では、この処理を書かないとService Workerが有効と判定されないようです
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches
+            .match(e.request)
+            .then(function(response) {
+                return response ? response : fetch(e.request);
+            })
+    )
+});
